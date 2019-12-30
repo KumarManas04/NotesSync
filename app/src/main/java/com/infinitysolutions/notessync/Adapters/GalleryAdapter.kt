019e041a524bc762e0e2f76b13d8d097733297ba
inf190 <kumarmanas04@gmail.com>
2019-12-30 23:39:07 +0530

    Add zooming ability to gallery images

--- app/src/main/java/com/infinitysolutions/notessync/Adapters/GalleryAdapter.kt
+++ app/src/main/java/com/infinitysolutions/notessync/Adapters/GalleryAdapter.kt
@@ -1,27 +1,59 @@
 package com.infinitysolutions.notessync.Adapters
 
 import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.view.MotionEvent
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.infinitysolutions.notessync.Model.ImageData
-import com.infinitysolutions.notessync.R
 import com.infinitysolutions.notessync.ViewModel.DatabaseViewModel
-import kotlinx.android.synthetic.main.image_list_item.view.image_view
+import com.ortiz.touchview.TouchImageView
 import java.io.File
 
 class GalleryAdapter(private val context: Context, private val list: ArrayList<ImageData>, private val databaseViewModel: DatabaseViewModel): RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(context).inflate(R.layout.view_page_layout, parent, false)
-        return ViewHolder(itemView)
+        return ViewHolder(TouchImageView(parent.context).apply {
+            layoutParams = ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+
+            setOnTouchListener { view, event ->
+                var result = true
+                //can scroll horizontally checks if there's still a part of the image
+                //that can be scrolled until you reach the edge
+                if (event.pointerCount >= 2 || view.canScrollHorizontally(1) && canScrollHorizontally(-1)) {
+                    //multi-touch event
+                    result = when (event.action) {
+                        MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                            // Disallow RecyclerView to intercept touch events.
+                            parent.requestDisallowInterceptTouchEvent(true)
+                            // Disable touch on view
+                            false
+                        }
+                        MotionEvent.ACTION_UP -> {
+                            // Allow RecyclerView to intercept touch events.
+                            parent.requestDisallowInterceptTouchEvent(false)
+                            true
+                        }
+                        else -> true
+                    }
+                }
+                result
+            }
+        })
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Glide.with(context).load(File(list[position].imagePath)).into(holder.imageView)
+        Glide.with(context).asBitmap().load(File(list[position].imagePath)).into(object : CustomTarget<Bitmap>(){
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                holder.imageView.setImageBitmap(resource)
+            }
+            override fun onLoadCleared(placeholder: Drawable?) {}
+        })
     }
 
     fun deleteImage(position: Int){
@@ -30,8 +62,8 @@ class GalleryAdapter(private val context: Context, private val list: ArrayList<I
         notifyItemRemoved(position)
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        val imageView: ImageView = itemView.image_view
+    class ViewHolder(view: TouchImageView): RecyclerView.ViewHolder(view){
+        val imageView = view
     }
 
     override fun getItemCount(): Int {
